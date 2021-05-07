b01f1e67775ad0e8a9ca92342fd4495c8058ccd3
snoor_madih <snoor.madih@gmail.com>
2021-05-07 18:42:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.getElementById('main').remove();
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "Snoor is the champion";
+document.body.appendChild(newHeader);
