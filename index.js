bc3fd9b0e26b8e8ff0e54add27be969d20b2f227
snoor_madih <snoor.madih@gmail.com>
2021-05-07 18:42:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-
-
 document.getElementById('main').remove();
 var newHeader = document.createElement('h1');
 newHeader.setAttribute("id","victory");
-newHeader.innerHTML = "LUKKAAAA is the champion";
+newHeader.innerHTML = "Snoor is the champion";
 document.body.appendChild(newHeader);
