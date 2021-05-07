39d0702c40a0c1ef5947b7d7e2836ec24b3ceae8
snoor_madih <snoor.madih@gmail.com>
2021-05-07 18:40:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// // Write your code here!
+// let name = document.getElementById("main");
+// name.remove();
+// let h1 = document.createElement("h1");
+// h1.setAtribute("id","victory");
+// let newHeader = document.getElementById("victory");
+// // newHeader.innerHTML ="YOUR-NAME is the champion";
